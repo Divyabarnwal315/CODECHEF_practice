997bb1d8c9a4ff5cd91b267942ab77bf69f49d2b
divya <divyabarnwal315@gmail.com>
2022-05-27 19:54:34 +0530

    DAY2

---
+++ DAY2/CRED_Coins.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int X,Y;
+        cin>>X>>Y;
+        cout<<(X*Y/100)<<endl;
+    }
+	return 0;
+}
