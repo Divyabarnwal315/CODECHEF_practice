6cdcefe2421c8780edcb697a6ea542d4776e61be
divya <divyabarnwal315@gmail.com>
2022-05-30 00:47:02 +0530

    DAY5

---
+++ DAY5/The_Preparations.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+{
+    int M,N,K;
+    cin>>M>>N>>K;
+    if(N*K<M)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+
+return 0;
+}
