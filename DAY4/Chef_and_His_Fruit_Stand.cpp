d171d0685d35614cb42a833e91002fdad168ad46
divya <divyabarnwal315@gmail.com>
2022-05-29 23:02:46 +0530

    DAY4

---
+++ DAY4/Chef_and_His_Fruit_Stand.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+{
+    int X,Y;
+    cin>>X>>Y;
+    cout<<min((X/2),Y)<<endl;
+}
+return 0;
+}
---
+++ DAY4/Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N;
+    while (true)
+    {
+        cin >> N;
+         if (N == 42)
+         break;
+         cout<<N<<endl;
+    }
+    return 0;
+}
