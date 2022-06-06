a5009b6e3295663397b0245dcb54be4aa1030970
divya <divyabarnwal315@gmail.com>
2022-06-06 16:54:12 +0530

    DAY12

---
+++ DAY12/Group_Assignment.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+{
+    int N,X;
+    cin>>N>>X;
+    cout<<((2*N)-X)+1<<endl;    
+}
+return 0;
+}
