adea77ed2ab41516390e9f4ff7bc931e513ab044
divya <divyabarnwal315@gmail.com>
2022-06-10 02:04:06 +0530

    DAY15

---
+++ DAY15/How_much_Scholarship.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int R;
+    cin>>R;
+    if(R>=1 && R<=50)
+    {
+        cout<<100<<endl;
+    }
+    else if(R>=51 && R<=100)
+    {
+        cout<<50<<endl;
+    }
+    else
+    {
+        cout<<0<<endl;
+    }
+return 0;
+}
