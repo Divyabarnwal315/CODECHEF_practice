4bc8ed42764c8d75a33073a49b99c1e746529aa5
divya <divyabarnwal315@gmail.com>
2022-06-11 00:07:17 +0530

    DAY16

---
+++ Elections_in_Chefland.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>50)
+    {
+        cout<<"A"<<
+    }
+}
+return 0;
+}
