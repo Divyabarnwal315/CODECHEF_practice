525551544535cb33c74e64f8663d9ee61813dc0f
divya <divyabarnwal315@gmail.com>
2022-07-02 05:26:55 +0530

    DAY32

---
+++ DAY32/gcd.cpp
@@ -0,0 +1,22 @@
+//program to find gcd of two numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m,n;
+    cout<<"Enter two numbers ";
+    cin>>m>>n;
+    while(m!=n)
+    {
+    if(m>n)
+    {
+        m=m-n;
+    }
+    else if(n>m)
+    {
+        n=n-m;
+    }
+    }
+    cout << "GCD is " <<m<< endl;
+    return 0;
+}
