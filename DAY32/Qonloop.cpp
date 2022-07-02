a805e11275bf6dd4abcf639116cb169d70e4c27c
divya <divyabarnwal315@gmail.com>
2022-07-02 05:33:13 +0530

    DAY32

---
+++ DAY32/Qonloop.cpp
@@ -0,0 +1,30 @@
+// Write a program which takes an integer from user and
+// that integer is reversed.If the reversed integer is equal to
+// the integer entered by user then,that number is a palindrome
+// if not that number is not a palindrome.
+//PROGRAM TO CHECK IF THE NO. IS PALINDROME OR NOT
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, num, digit, rev = 0;
+    cout << "Enter a positive number: ";
+    cin >> num;
+    n = num;
+    do
+    {
+        digit = num % 10;
+        rev = (rev * 10) + digit;
+        num = num / 10;
+    } while (num != 0);
+    cout << " The reverse of the number is: " << rev << endl;
+    if (n == rev)
+    {
+        cout << " The number is a palindrome";
+    }
+    else
+    {
+        cout << " The number is not a palindrome";
+    }
+    return 0;
+}
