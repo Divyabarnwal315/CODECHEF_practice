2fc55f3bb6330cfb52159629f6b09fb390a6453a
divya <divyabarnwal315@gmail.com>
2022-11-08 15:31:31 +0530

    DAY55

---
+++ DAY55/Paper_Cutting.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int K,N;
+        cin>>K>>N;
+        cout<<(K/N)*(K/N)<<endl;
+    }
+return 0;
+}
