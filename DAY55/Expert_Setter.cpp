c1f80705434f7a7f77b5550fade4974f21852c2b
divya <divyabarnwal315@gmail.com>
2022-11-08 15:18:32 +0530

    DAY55

---
+++ DAY55/Expert_Setter.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        float X,Y;
+        cin>>X>>Y;
+        if((X/2)<=Y)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+return 0;
+}
