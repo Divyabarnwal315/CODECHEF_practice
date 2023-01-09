92919af04e4cbf83d4d256d9c99074c4c0d713f8
divya <divyabarnwal315@gmail.com>
2023-01-09 13:04:02 +0530

    DAY58

---
+++ DAY58/Sum_it.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        if((A+B)==C)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+return 0;
+}
