5cd90c11ac4077eee57611f1b043de705636a2a5
divya <divyabarnwal315@gmail.com>
2023-01-12 02:26:33 +0530

    DAY60

---
+++ DAY60/Chef_Plays_Ludo.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int X;
+        cin>>X;
+        if(X==6)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+return 0;
+}
