a9a6c3adccd647c4135ead34e3d71e651493c348
divya <divyabarnwal315@gmail.com>
2023-03-06 01:39:12 +0530

    DAY70

---
+++ DAY70/Air_Hockey.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+{
+    int A,B;
+    cin>>A>>B;
+    // if(A==B)
+    // {
+    //     cout<<7<<endl;
+    // }
+    // else
+    // {
+        cout<<abs(7-max(A,B))<<endl;
+    // }
+}
+return 0;
+}
