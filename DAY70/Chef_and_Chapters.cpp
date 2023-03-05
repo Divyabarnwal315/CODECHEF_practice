52d8575d7b1a11d56ca342a00c093e646bf88f77
divya <divyabarnwal315@gmail.com>
2023-03-06 01:43:26 +0530

    DAY70

---
+++ DAY70/Chef_and_Chapters.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+        cout << (X * Y) * Z << endl;
+    }
+    return 0;
+}
