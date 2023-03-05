6bf870f526d8b2ef40c6a59b40ee03fc822e1e4e
divya <divyabarnwal315@gmail.com>
2023-03-06 01:38:45 +0530

    DAY70

---
+++ DAY70/AI_Analysing_Code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int X;
+    cin>>X;
+    if(X<=1000)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+return 0;
+}
