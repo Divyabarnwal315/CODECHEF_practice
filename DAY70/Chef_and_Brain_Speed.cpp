6b347052b29083319699fcc265040938cea574aa
divya <divyabarnwal315@gmail.com>
2023-03-06 01:42:53 +0530

    DAY70

---
+++ DAY70/Chef_and_Brain_Speed.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+//     int t;
+//     cin>>t;
+//     while(t--)
+// {
+    int X,Y;
+    cin>>X>>Y;
+    if(Y>X)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+// }
+return 0;
+}
