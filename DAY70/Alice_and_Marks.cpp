44f8eca8fa929b694aa62b15ff61fd993a5f26f8
divya <divyabarnwal315@gmail.com>
2023-03-06 01:43:51 +0530

    DAY70

---
+++ DAY70/Alice_and_Marks.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int X,Y;
+    cin>>X>>Y;
+    if(X>=(2*Y))
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+return 0;
+}
