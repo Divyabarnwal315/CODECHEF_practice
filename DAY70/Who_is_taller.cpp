d73161073a0a4bd4946fd45b6c6bd5fbb5fcfc2d
divya <divyabarnwal315@gmail.com>
2023-03-06 01:51:33 +0530

    DAY70

---
+++ DAY70/Who_is_taller.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+{
+    int X,Y;
+    cin>>X>>Y;
+    if(X>Y)
+    {
+        cout<<"A"<<endl;
+    }
+    else
+    {
+        cout<<"B"<<endl;
+    }
+}
+return 0;
+}
