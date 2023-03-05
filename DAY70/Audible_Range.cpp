5c3a174eab8ceb860282955f5dcd4ef59c73f9e7
divya <divyabarnwal315@gmail.com>
2023-03-06 01:39:45 +0530

    DAY70

---
+++ DAY70/Audible_Range.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+{
+    int X;
+    cin>>X;
+    if(X>=67 && X<=45000)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+return 0;
+}
