966a8dafc1f1ac1b06fa6849a11a469f61b770ea
divya <divyabarnwal315@gmail.com>
2023-03-07 09:43:12 +0530

    DAY71

---
+++ DAY71/Chef_in_his_Office.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+{
+    int X,Y;
+    cin>>X>>Y;
+    cout<<(4*X)+Y<<endl;
+}
+return 0;
+}
