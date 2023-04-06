a8748d5dc0a11ceecf81e32c8f37fe16048735a8
divya <divyabarnwal315@gmail.com>
2023-04-06 20:14:25 +0530

    DAY72

---
+++ DAY72/Chef_and_Donation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int X, Y;
+        cin >> X >> Y;
+        cout << Y - X << endl;
+    }
+    return 0;
+}
