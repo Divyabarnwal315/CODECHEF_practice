90e8c26fbb502f4981292b975921de4399ccd8aa
divya <divyabarnwal315@gmail.com>
2023-04-06 20:50:27 +0530

    DAY72

---
+++ DAY72/Just_One_More_Episode.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int X;
+        cin>>X;
+        if(X<=24)
+        {
+            cout<<"No"<<endl;
+        }
+        else
+        {
+            cout<<"Yes"<<endl;
+        }
+    }
+return 0;
+}
