893f05c7d25f88f2a242661e7546ff21ec65eca5
divya <divyabarnwal315@gmail.com>
2023-04-07 12:31:39 +0530

    DAY73

---
+++ DAY73/Couple_Game.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int G, B;
+        cin >> G >> B;
+        cout << B - G << endl;
+    }
+    return 0;
+}
