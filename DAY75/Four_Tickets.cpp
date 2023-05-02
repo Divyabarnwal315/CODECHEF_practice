e35f13aa1f05130d9949da4de7e0f457ca0be92e
divya <divyabarnwal315@gmail.com>
2023-05-02 21:28:15 +0530

    DAY75

---
+++ DAY75/Four_Tickets.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int X;
+        cin>>X;
+        if((X*4)<=(1000))
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+return 0;
+}
