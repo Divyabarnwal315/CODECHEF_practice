f3e9acaa54224fc26912afebfbb33951586087be
divya <divyabarnwal315@gmail.com>
2023-05-02 21:41:18 +0530

    DAY75

---
+++ DAY75/Buy_Ten_Packets.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int Y, X;
+        cin >> Y >> X;
+        cout << ((X * 2) + (Y)) << endl;
+    }
+    return 0;
+}
