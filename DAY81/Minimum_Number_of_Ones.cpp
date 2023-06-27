fa084df8b09285d44f76a49023ff3454380c8bf8
divya <divyabarnwal315@gmail.com>
2023-06-27 13:18:16 +0530

    DAY81

---
+++ DAY81/Minimum_Number_of_Ones.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        cout<<N/2<<endl;
+    }
+return 0;
+}
